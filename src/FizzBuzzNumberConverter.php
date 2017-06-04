bec06f872e4c327081e3f22fc79c010c7c94de5c
Alexander Sousa <>
2017-06-03 21:47:10 -0400

    Refactored the converter to use no if statements, it also receives the messages it should print in its constructor. Increased the amount of tests. This converter should meet the requirements of the Backend Developer test

--- src/FizzBuzzNumberConverter.php
+++ src/FizzBuzzNumberConverter.php
@@ -2,17 +2,46 @@
 namespace anesed\fizzbuzz;
 
 class FizzBuzzNumberConverter {
+    const MULTIPLICITY_NONE = 0;
+    const MULTIPLICITY_THREE = 1;
+    const MULTIPLICITY_FIVE = 2;
+    const MULTIPLICITY_BOTH = 3;    
+    
+    private $conversionMethods;
+    
+    public function __construct(string $messageMultipleOfThree, 
+            string $messageMultipleOfFive, 
+            string $messageMultipleOfBoth) {
+        $this->conversionMethods = [
+            self::MULTIPLICITY_NONE => function($number) {
+                return (string)$number;
+            },
+            self::MULTIPLICITY_THREE => function($number) use ($messageMultipleOfThree) {
+                return $messageMultipleOfThree;
+            },
+            self::MULTIPLICITY_FIVE => function($number) use ($messageMultipleOfFive) {
+                return $messageMultipleOfFive;
+            },
+            self::MULTIPLICITY_BOTH => function($number) use ($messageMultipleOfBoth) {
+                return $messageMultipleOfBoth;
+            }
+        ];
+    }
     
     public function convert(int $number) : string {
-        $fizzBuzzValue = sprintf("%d", $number);
-        if ($number % 5 == 0 && $number % 3 == 0) {
-            $fizzBuzzValue = "Linianos";
-        } else if ($number % 5 == 0) {
-            $fizzBuzzValue = "IT";
-        } else if ($number % 3 == 0) {
-            $fizzBuzzValue = "Linio";
-        }
-        return $fizzBuzzValue;
+        $multiplicity = $this->getMultiplicity($number);
+        $conversionMethod = $this->conversionMethods[$multiplicity];
+        return $conversionMethod($number);
+    }
+    
+    private function getMultiplicity(int $number) : int {
+        $multipleOfFive = $this->getMultiplicityBit($number, 5) * 2;
+        $multipleOfThree = $this->getMultiplicityBit($number, 3);
+        return $multipleOfFive + $multipleOfThree;
+    }
+    
+    private function getMultiplicityBit(int $number, int $bit) : int {
+        return max(0, 1 - abs($number % $bit));
     }
     
 }
--- tests/FizzBuzzConverterTest.php
+++ tests/FizzBuzzConverterTest.php
@@ -5,24 +5,54 @@ use \anesed\fizzbuzz\FizzBuzzNumberConverter;
 class FizzBuzzConverterTest extends TestCase {
     private $converter;
     
+    private $messageThree = "Linio";
+    private $messageFive = "IT";
+    private $messageBoth = "Linianos";
+    
     protected function setUp() {
-        $this->converter = new FizzBuzzNumberConverter();
+        $this->converter = new FizzBuzzNumberConverter($this->messageThree, 
+                $this->messageFive, 
+                $this->messageBoth);
     }
     
-    public function testMultipleOfFive() {
-        $this->assertEquals($this->converter->convert(5), "IT");
+    public function testFive() {
+        $this->assertEquals($this->converter->convert(5), $this->messageFive);
     }
     
-    public function testMultipleOfThree() {
-        $this->assertEquals($this->converter->convert(3), "Linio");
+    public function testLargerMultipleOfFive() {
+        $this->assertEquals($this->converter->convert(50000), $this->messageFive);
     }
     
-    public function testMultipleOfBothThreeAndFive() {
-        $this->assertEquals($this->converter->convert(15), "Linianos");
+    public function testThree() {
+        $this->assertEquals($this->converter->convert(3), $this->messageThree);
+    }
+    
+    public function testLargerMultipleOfThree() {
+        $this->assertEquals($this->converter->convert(60003), $this->messageThree);
+    }
+    
+    public function testThreeTimesFive() {
+        $this->assertEquals($this->converter->convert(15), $this->messageBoth);
+    }
+    
+    public function testLargerMultipleOfBoth() {
+        $this->assertEquals($this->converter->convert(50115), $this->messageBoth);
     }
     
     public function testMultipleOfNeitherThreeOrFive() {
+        $this->assertEquals($this->converter->convert(8), "8");
+    }
+    
+    public function testLargerMultipleOfNeitherThreeOrFive() {
+        $this->assertEquals($this->converter->convert(50972), "50972");
+    }
+    
+    public function testPrime() {
         $this->assertEquals($this->converter->convert(7), "7");
+    }
+    
+    public function testLargerPrime() {
+        $this->assertEquals($this->converter->convert(50971), "50971");
     }
     
 }
